4e1e1913d19a6375f19b88a23c0fc612db9c1dac
syeda <sbtshakeel61@gmail.com>
2024-06-04 18:57:50 +0500

    rest operators in string

---
+++ app.ts
@@ -0,0 +1,8 @@
+//rest operator in string to to works
+function str (name : string , ...teacherName:string[]){ 
+    console.log(`Hello ${name},${teacherName}`)
+}
+str("Zara", "Sabahat","Farah", "Saba")// and so on...
+
+
+
